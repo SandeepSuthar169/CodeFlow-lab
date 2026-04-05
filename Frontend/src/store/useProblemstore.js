ff9d54073f9c45c8f2d6b98900d4c311cd301563
Sandeep Suthar <ss.suthar.004@gmail.com>
2026-04-05 15:05:11 +0530

    useProblemStore added api

--- Frontend/src/store/useProblemstore.js
+++ Frontend/src/store/useProblemstore.js
@@ -22,12 +22,49 @@ const useProblemStore = create((set) => ({
             
             set({ problem: res.data.problems })
         } catch (error) {
-            
+            console.log("Error getting all problem", error);
+            toast.error("Error in getting")
+        } finally {
+            set({ isProblemsLoading: false })
+        }   
+    
+    },
+
+    getProblemById: async () => {
+        try {
+            set({ isProblemLoading: true })
+
+            const res = await axiosInstance.get(`/problem/get-problem/${id}`)
+
+
+            console.log("res", res);
+            console.log("res.data", res.data);
+            console.log("res.data.problems", res.data.problems);
+
+            set({ problem: res.data.problems })
+            toast.success(res.data.message)
+        } catch (error) {
+            console.log("Error getting  problem", error);
+            toast.error("Error in getting problem")
+        } finally {
+            set({ isProblemLoading: false })
         }
     },
 
-    getProblemById: async () => {},
+    getSolvedProblemByUser: async () => {
+        try {
+            const res = await axiosInstance.get("/get-solved-problem")
 
-    getSolvedProblemByUser: async () => {}
+            console.log("res", res);
+            console.log("res.data", res.data);
+            console.log("res.data.problems", res.data.problems);
+
+            set({ solvedProblem: res.data.problems })
+            toast.success(res.data.message)
+        } catch (error) {
+            console.log("Error solving problem", error);
+            toast.error("Error solveing problem")
+        } 
+    }
 }))
 
