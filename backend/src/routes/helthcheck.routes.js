c0d35d53320982d610d477f45cb17de770c989ff
Sandeep Suthar <ss.suthar.004@gmail.com>
2026-08-11 15:57:49 +0530

    restart project

--- backend/src/routes/helthcheck.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { helthCheck } from "../controllers/healthcheck.controllers.js"
-
-const router = Router()
-
-router.route("/").get(helthCheck)
-
-export default router
